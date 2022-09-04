86b2e99d3619bae8ad5d9258a06ebad87ae47992
Stefano Pigozzi <me@steffo.eu>
2022-09-05 00:16:44 +0200

    Define initial tables

---
+++ lokiunimore/sql/__init__.py
@@ -0,0 +1,54 @@
+import sqlalchemy as s
+import sqlalchemy.orm as o
+
+
+Base = o.declarative_base()
+
+
+class User(Base):
+    """
+    A Matrix user.
+    """
+
+    __tablename__ = "users"
+
+    id = s.Column(s.String, primary_key=True)
+
+    tokens = o.relationship("User", back_populates="user")
+    authorizations = o.relationship("Authorization", back_populates="user")
+
+
+class Token(Base):
+    """
+    A token issued to an user who joined the parent Matrix space.
+    """
+
+    __tablename__ = "tokens"
+
+    user_id = s.Column(s.String, s.ForeignKey("users.id"), nullable=False)
+    token = s.Column(s.String, primary_key=True)
+
+    user = o.relationship("User", back_populates="tokens")
+
+
+class Authorization(Base):
+    """
+    A successful authorization through the bot, which allows the user to join the child Matrix space.
+    """
+
+    __tablename__ = "authorizations"
+
+    user_id = s.Column(s.String, s.ForeignKey("users.id"), nullable=False)
+    first_name = s.Column(s.String, nullable=False)
+    last_name = s.Column(s.String, nullable=False)
+    email = s.Column(s.String, nullable=False)
+    published = s.Column(s.Boolean, nullable=False)
+
+    user = o.relationship("User", back_populates="authorizations")
+
+
+__all__ = (
+    "User",
+    "Token",
+    "Authorization",
+)
